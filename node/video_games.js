5b246f66a3de0544fb3d133b64842e4cfcfe8115
James Skemp <strivinglife@gmail.com>
2021-09-05 23:26:25 -0500

    Update video games XML to JSON script
    Should be easier to parse in the Angular app I'm developing.

--- node/video_games.js
+++ node/video_games.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
+const stringKeys = ['title', 'console', 'version', 'price', 'notes'];
 
 const xmlFile = fs.readFile('./xml/video_games.xml', (err, data) => {
 	if (err) {
@@ -13,7 +14,7 @@ const xmlFile = fs.readFile('./xml/video_games.xml', (err, data) => {
 			throw err;
 		}
 
-		fs.writeFile('./json/video_games.json', JSON.stringify(result, null, '\t'), (err) => {
+		fs.writeFile('./json/video_games.json', JSON.stringify(result, videoGameXmlReplacer, '\t'), (err) => {
 			if (err) {
 				throw err;
 			}
@@ -21,3 +22,44 @@ const xmlFile = fs.readFile('./xml/video_games.xml', (err, data) => {
 		});
 	});
 });
+
+function videoGameXmlReplacer(key, value) {
+	if (value && typeof value === 'object' && value.hasOwnProperty('$') && !value.hasOwnProperty('game')) {
+		// For the game elements, replace the generated $ with meta.
+		value['meta'] = value['$'];
+	}
+	if (stringKeys.includes(key)) {
+		if (key === "notes") {
+			return value.toString().replace(/\s+/g, ' ').trim();
+		}
+		return value.toString();
+	} else if (key === "date") {
+		const stringValue = value.toString();
+		if (stringValue === '[object Object]') {
+			return null;
+		}
+		return stringValue;
+	} else if (key === "own") {
+		return value === "yes";
+	} else if (key === "addOn" || key === "electronic" || key === "beat" || key === "used") {
+		return value === "true";
+	} else if (key === "system" || key === "purchase" || key === "sell") {
+		return value[0];
+	} else if (key === "place") {
+		if (value[0].hasOwnProperty('_')) {
+			return value[0]['_'];
+		}
+		return value[0];
+	} else if (key === "id") {
+		return Number(value);
+	} else if (key === "$") {
+		if (value.hasOwnProperty("xmlns:xsi")) {
+			return undefined;
+		}
+		// We changed it to 'meta' above, so drop the game attributes as well.
+		return undefined;
+	} else if (key === "xmlns:xsi" || key === "xsi:noNamespaceSchemaLocation") {
+		return undefined;
+	}
+	return value;
+}
